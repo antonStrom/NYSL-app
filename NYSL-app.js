a6b1252345fb79662c4bfbb8f1ae50abee2b98fb
antonStrom <anton-strom@hotmail.com>
2019-10-18 09:35:50 +0200

    push push

--- NYSL-app.js
+++ NYSL-app.js
@@ -1,7 +1,51 @@
 const app = new Vue({
     el: '#app',
-    data: {},
-    created() {},
-    methods: {},
-    computed: {}
+    data: {
+        teams: [{
+                team: "U1",
+                home_field: "AJ Katzenmaier",
+                id: "111",
+                logo: ""
+            },
+            {
+                team: "U2",
+                home_field: "Greenbay",
+                id: "222",
+                logo: ""
+            },
+            {
+                team: "U3",
+                home_field: "Howard A Yeager",
+                id: "333",
+                logo: ""
+            },
+            {
+                team: "U4",
+                home_field: "Marjorie P Hart",
+                id: "444",
+                logo: ""
+            },
+            {
+                team: "U5",
+                home_field: "North",
+                id: "555",
+                logo: ""
+            },
+            {
+                team: "U6",
+                home_field: "South",
+                id: "666",
+                logo: ""
+            }
+        ]
+    },
+    created() {
+
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
 });
