e152abc08c945d62477d206c7da4b40d5b1113fc
antonStrom <anton-strom@hotmail.com>
2019-10-24 16:40:50 +0200

    game schedule fixed, started with result page

--- NYSL-app.js
+++ NYSL-app.js
@@ -9,25 +9,11 @@ const app = new Vue({
                 time: "9:30AM"
             },
             {
-                date: "01/9/2019",
-                home_team: "U3",
-                away_team: "U2",
-                field: "Howard A Yeager",
-                time: "1:00PM"
-            },
-            {
                 date: "08/9/2019",
                 home_team: "U5",
                 away_team: "U6",
                 field: "North",
                 time: "9:30AM"
-            },
-            {
-                date: "08/9/2019",
-                home_team: "U6",
-                away_team: "U1",
-                field: "South",
-                time: "1:00PM"
             },
             {
                 date: "15/9/2019",
@@ -37,25 +23,11 @@ const app = new Vue({
                 time: "9:30AM"
             },
             {
-                date: "15/9/2019",
-                home_team: "U3",
-                away_team: "U5",
-                field: "Howard A Yeager",
-                time: "1:00PM"
-            },
-            {
                 date: "22/9/2019",
                 home_team: "U1",
                 away_team: "U3",
                 field: "AJ Katzenmaier",
                 time: "9:30AM"
-            },
-            {
-                date: "22/9/2019",
-                home_team: "U2",
-                away_team: "U6",
-                field: "Greenbay",
-                time: "1:00PM"
             },
             {
                 date: "29/9/2019",
@@ -72,25 +44,11 @@ const app = new Vue({
                 time: "9:30AM"
             },
             {
-                date: "06/10/2019",
-                home_team: "U1",
-                away_team: "U6",
-                field: "AJ Katzenmaier",
-                time: "1:00PM"
-            },
-            {
                 date: "08/10/2019",
                 home_team: "U3",
                 away_team: "U4",
                 field: "Howard A Yeager",
                 time: "9:30AM"
-            },
-            {
-                date: "08/10/2019",
-                home_team: "U5",
-                away_team: "U1",
-                field: "North",
-                time: "1:00PM"
             },
             {
                 date: "20/10/2019",
@@ -100,25 +58,132 @@ const app = new Vue({
                 time: "9:30AM"
             },
             {
-                date: "20/10/2019",
-                home_team: "U2",
-                away_team: "U4",
-                field: "Greenbay",
-                time: "1:00PM"
-            },
-            {
                 date: "27/10/2019",
                 home_team: "U3",
                 away_team: "U1",
                 field: "Howard A Yeager",
                 time: "9:30AM"
+            }
+        ],
+        schedulePM: [{
+                date: "01/9/2019",
+                home_team: "U3",
+                vs: "VS.",
+                away_team: "U2",
+                field: "Location: Howard A Yeager",
+                time: "1:00PM"
+            },
+            {
+                date: "08/9/2019",
+                home_team: "U6",
+                vs: "VS.",
+                away_team: "U1",
+                field: "Location: South",
+                time: "1:00PM"
+            },
+            {
+                date: "15/9/2019",
+                home_team: "U3",
+                vs: "VS.",
+                away_team: "U5",
+                field: "Location: Howard A Yeager",
+                time: "1:00PM"
+            },
+            {
+                date: "22/9/2019",
+                home_team: "U2",
+                vs: "VS.",
+                away_team: "U6",
+                field: "Location: Greenbay",
+                time: "1:00PM"
+            },
+            {
+                date: "",
+                home_team: "",
+                away_team: "",
+                field: "",
+                time: ""
+            },
+            {
+                date: "06/10/2019",
+                home_team: "U1",
+                vs: "VS.",
+                away_team: "U6",
+                field: "Location: AJ Katzenmaier",
+                time: "1:00PM"
+            },
+            {
+                date: "08/10/2019",
+                home_team: "U5",
+                vs: "VS.",
+                away_team: "U1",
+                field: "Location: North",
+                time: "1:00PM"
+            },
+            {
+                date: "20/10/2019",
+                home_team: "U2",
+                vs: "VS.",
+                away_team: "U4",
+                field: "Location: Greenbay",
+                time: "1:00PM"
             },
             {
                 date: "27/10/2019",
                 home_team: "U5",
+                vs: "VS.",
                 away_team: "U6",
-                field: "North",
+                field: "Location: North",
                 time: "1:00PM"
+            }
+        ],
+        result: [{
+                team: "U1",
+                games_played: "",
+                win: "",
+                tie: "",
+                loss: "",
+                points: ""
+            },
+            {
+                team: "U2",
+                games_played: "",
+                win: "",
+                tie: "",
+                loss: "",
+                points: ""
+            },
+            {
+                team: "U3",
+                games_played: "",
+                win: "",
+                tie: "",
+                loss: "",
+                points: ""
+            },
+            {
+                team: "U4",
+                games_played: "",
+                win: "",
+                tie: "",
+                loss: "",
+                points: ""
+            },
+            {
+                team: "U5",
+                games_played: "",
+                win: "",
+                tie: "",
+                loss: "",
+                points: ""
+            },
+            {
+                team: "U6",
+                games_played: "",
+                win: "",
+                tie: "",
+                loss: "",
+                points: ""
             }
         ],
         location: [{
@@ -145,7 +210,9 @@ const app = new Vue({
                 field: 'South',
                 adress: '8255 S Houston Ave, Chicago, IL 60617'
             }
-        ]
+        ],
+        n: 0,
+        j: 0,
     },
     created() {
 
@@ -155,10 +222,10 @@ const app = new Vue({
     },
     computed: {
         gameDayAM() {
-            return this.schedule[0]
+            return this.schedule
         },
         gameDayPM() {
-            return this.schedule[1]
+            return this.schedulePM
         }
     }
 });
